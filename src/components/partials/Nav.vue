c1663813bccfc35da7fd0e8f221ea5acb9be4304
unknown <jerexbambex@gmail.com>
2020-06-29 16:10:15 -0700

    Included the css and js dependencies and also started with the homepage design

---
+++ src/components/partials/Nav.vue
@@ -0,0 +1,220 @@
+<template>
+	<!-- Navbar STart -->
+        <header id="topnav" class="defaultscroll sticky bg-white">
+            <div class="container">
+                <!-- Logo container-->
+                <div>
+                    <a class="logo" href="index.html">
+                        <!-- <img src="images/logo-dark.png" height="24" alt=""> -->
+                        Fegocoosa
+                    </a>
+                </div>
+                <div class="buy-button">
+                    <a href="https://1.envato.market/4n73n" target="_blank" class="btn btn-primary">Buy Now</a>
+                </div><!--end login button-->
+                <!-- End Logo container-->
+                <div class="menu-extras">
+                    <div class="menu-item">
+                        <!-- Mobile menu toggle-->
+                        <a class="navbar-toggle"  @click="isOpen = !isOpen">
+                            <div class="lines">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </a>
+                        <!-- End mobile menu toggle-->
+                    </div>
+                </div>
+
+                <div id="navigation" v-show="isOpen">
+                    <!-- Navigation Menu-->
+                    <ul class="navigation-menu">
+                        <li><a href="index.html">Home</a></li>
+                        <li class="has-submenu">
+                            <a href="javascript:void(0)">Landing</a><span class="menu-arrow"></span>
+                            <ul class="submenu megamenu">
+                                <li>
+                                    <ul>
+                                        <li><a href="index-saas.html">Saas</a></li>
+                                        <li><a href="index-classic-saas.html">Classic Saas</a></li>
+                                        <li><a href="index-agency.html">Agency</a></li>
+                                        <li><a href="index-apps.html">Application</a></li>
+                                        <li><a href="index-classic-app.html">Classic Application</a></li>
+                                        <li><a href="index-studio.html">Studio</a></li>
+                                        <li><a href="index-business.html">Business</a></li>
+                                        <li><a href="index-modern-business.html">Modern Business</a></li>
+                                        <li><a href="index-hotel.html">Hotel</a></li>
+                                        <li><a href="index-marketing.html">Marketing</a></li>
+                                        <li><a href="index-enterprise.html">Enterprise </a></li>
+                                    </ul>
+                                </li>
+
+                                <li>
+                                    <ul>
+                                        <li><a href="index-insurance.html">Insurance <span class="badge badge-danger rounded">v 2.2</span></a></li>
+                                        <li><a href="index-shop.html">Shop <span class="badge badge-danger rounded">v 2.2</span></a></li>
+                                        <li><a href="index-ebook.html">E-Book <span class="badge badge-danger rounded">v 2.2</span></a></li>
+                                        <li><a href="index-coworking.html">Coworking</a></li>
+                                        <li><a href="index-personal.html">Personal </a></li>
+                                        <li><a href="index-services.html">Service </a></li>
+                                        <li><a href="index-payments.html">Payments </a></li>
+                                        <li><a href="index-crypto.html">Cryptocurrency </a></li>
+                                        <li><a href="index-software.html">Software </a></li>
+                                        <li><a href="index-car-riding.html">Car Ride</a></li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <ul>
+                                        <li><a href="index-hosting.html">Hosting & Domain</a></li>
+                                        <li><a href="index-event.html">Event</a></li>
+                                        <li><a href="index-course.html">Course </a></li>
+                                        <li><a href="index-online-learning.html">Online Learning</a></li>
+                                        <li><a href="index-single-product.html">Product </a></li>
+                                        <li><a href="index-portfolio.html">Portfolio </a></li>
+                                        <li><a href="index-job.html">Job</a></li>
+                                        <li><a href="index-customer.html">Customer</a></li>
+                                        <li><a href="index-social-marketing.html">Social Media</a></li>
+                                        <li><a href="index-digital-agency.html">Digital Agency</a></li>
+                                        <li><a href="index-onepage.html">Saas <span class="badge badge-pill badge-warning ml-2">Onepage</span></a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li class="has-submenu">
+                            <a href="javascript:void(0)">Pages</a><span class="menu-arrow"></span>
+                            <ul class="submenu">
+                                <li><a href="page-aboutus.html"> About Us</a></li>
+                                <li><a href="page-services.html">Services</a></li>
+                                <li><a href="page-pricing.html">Pricing</a></li>
+                                <li><a href="page-team.html"> Team</a></li>
+                                <li class="has-submenu"><a href="javascript:void(0)"> Account</a><span class="submenu-arrow"></span>
+                                    <ul class="submenu">
+                                        <li><a href="page-profile.html">Profile</a></li>
+                                        <li><a href="page-profile-edit.html">Account Setting</a></li>
+                                        <li><a href="page-invoice.html">Invoice</a></li>
+                                    </ul>
+                                </li>
+                                <li class="has-submenu"><a href="javascript:void(0)"> Shop <span class="badge badge-danger rounded">v 2.2</span></a><span class="submenu-arrow"></span>
+                                    <ul class="submenu">
+                                        <li><a href="shop-products.html">All Products <span class="badge badge-primary rounded">new</span></a></li>
+                                        <li><a href="shop-product-detail.html">Product Details <span class="badge badge-primary rounded">new</span></a></li>
+                                        <li><a href="shop-cart.html">Shop Cart <span class="badge badge-primary rounded">new</span></a></li>
+                                        <li><a href="shop-checkouts.html">Checkouts <span class="badge badge-primary rounded">new</span></a></li>
+                                        <li><a href="shop-myaccount.html">My Account <span class="badge badge-primary rounded">new</span></a></li>
+                                    </ul>
+                                </li>
+                                <li class="has-submenu"><a href="javascript:void(0)"> Help center <span class="badge badge-danger rounded">v 2.2</span></a><span class="submenu-arrow"></span>
+                                    <ul class="submenu">
+                                        <li><a href="helpcenter-overview.html">Helpcenter <span class="badge badge-primary rounded">new</span></a></li>
+                                        <li><a href="helpcenter-faqs.html">Faqs <span class="badge badge-primary rounded">new</span></a></li>
+                                        <li><a href="helpcenter-guides.html">Guides <span class="badge badge-primary rounded">new</span></a></li>
+                                        <li><a href="helpcenter-support-request.html">Support Call <span class="badge badge-primary rounded">new</span></a></li>
+                                    </ul>
+                                </li>
+                                <li class="has-submenu"><a href="javascript:void(0)"> Email Template</a><span class="submenu-arrow"></span>
+                                    <ul class="submenu">
+                                        <li><a href="email-confirmation.html">Confirmation</a></li>
+                                        <li><a href="email-password-reset.html">Reset Password</a></li>
+                                        <li><a href="email-alert.html">Alert</a></li>
+                                        <li><a href="email-invoice.html">Invoice</a></li>
+                                    </ul>
+                                </li>
+                                <li class="has-submenu"><a href="javascript:void(0)">Careers</a><span class="submenu-arrow"></span>
+                                    <ul class="submenu">
+                                        <li><a href="page-jobs.html">Jobs</a></li>
+                                        <li><a href="page-jobs-sidebar.html">Jobs - Sidebar</a></li>
+                                        <li><a href="page-job-detail.html">Job Detail</a></li>
+                                        <li><a href="page-job-apply.html">Job Apply</a></li>
+                                        <li><a href="page-job-company.html">Company</a></li>
+                                        <li><a href="page-job-candidate.html">Candidate</a></li>
+                                    </ul>
+                                </li>
+                                <li class="has-submenu"><a href="javascript:void(0)"> Blog</a><span class="submenu-arrow"></span>
+                                    <ul class="submenu">
+                                        <li><a href="page-blog-grid.html">Blog Grid</a></li>
+                                        <li><a href="page-blog-sidebar.html">Blog with Sidebar</a></li>
+                                        <li><a href="page-blog-list.html">Blog Listing</a></li>
+                                        <li><a href="page-blog-list-sidebar.html">Blog List & Sidebar</a></li>
+                                        <li><a href="page-blog-detail.html">Blog Detail</a></li>
+                                    </ul>
+                                </li>
+                                <li class="has-submenu"><a href="javascript:void(0)"> Works</a><span class="submenu-arrow"></span>
+                                    <ul class="submenu">
+                                        <li><a href="page-work-modern.html">Works Modern</a></li>
+                                        <li><a href="page-work-classic.html">Works Classic</a></li>
+                                        <li><a href="page-work-grid.html">Works Grid</a></li>
+                                        <li><a href="page-work-masonry.html">Works Masonry</a></li>
+                                        <li><a href="page-work-detail.html">Work Detail</a></li>
+                                    </ul>
+                                </li>
+                                <li class="has-submenu"><a href="javascript:void(0)"> Auth Pages</a><span class="submenu-arrow"></span>
+                                    <ul class="submenu">
+                                        <li><a href="auth-login.html">Login</a></li>
+                                        <li><a href="auth-cover-login.html">Login Cover</a></li>
+                                        <li><a href="auth-login-three.html">Login Simple</a></li>
+                                        <li><a href="auth-signup.html">Signup</a></li>
+                                        <li><a href="auth-cover-signup.html">Signup Cover</a></li>
+                                        <li><a href="auth-signup-three.html">Signup Simple</a></li>
+                                        <li><a href="auth-re-password.html">Reset Password</a></li>
+                                        <li><a href="auth-cover-re-password.html">Reset Password Cover</a></li>
+                                        <li><a href="auth-re-password-three.html">Reset Password Simple</a></li>
+                                    </ul>
+                                </li>
+                                <li class="has-submenu"><a href="javascript:void(0)"> Utility </a><span class="submenu-arrow"></span>
+                                    <ul class="submenu">
+                                        <li><a href="page-terms.html">Terms of Services</a></li>
+                                        <li><a href="page-privacy.html">Privacy Policy</a></li>
+                                    </ul>
+                                </li>
+                                <li class="has-submenu"><a href="javascript:void(0)"> Special </a><span class="submenu-arrow"></span>
+                                    <ul class="submenu">
+                                        <li><a href="page-comingsoon.html">Coming Soon</a></li>
+                                        <li><a href="page-comingsoon2.html">Coming Soon Two </a></li>
+                                        <li><a href="page-maintenance.html">Maintenance</a></li>
+                                        <li><a href="page-error.html">Error</a></li>
+                                    </ul>
+                                </li>
+                                <li class="has-submenu"><a href="javascript:void(0)"> Contact </a><span class="submenu-arrow"></span>
+                                    <ul class="submenu">
+                                        <li><a href="page-contact-detail.html">Contact Detail </a></li>
+                                        <li><a href="page-contact-one.html">Contact One </a></li>
+                                        <li><a href="page-contact-two.html">Contact Two </a></li>
+                                        <li><a href="page-contact-three.html">Contact Three </a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="has-submenu">
+                            <a href="javascript:void(0)">Docs</a><span class="menu-arrow"></span>
+                            <ul class="submenu">
+                                <li><a href="documentation.html">Documentation </a></li>
+                                <li><a href="changelog.html">Changelog </a></li>
+                                <li><a href="components.html">Components</a></li>
+                                <li><a href="widget.html">Widget</a></li>
+                            </ul>
+                        </li>
+                    </ul><!--end navigation menu-->
+                    <div class="buy-menu-btn d-none">
+                        <a href="https://1.envato.market/4n73n" target="_blank" class="btn btn-primary">Buy Now</a>
+                    </div><!--end login button-->
+                </div><!--end navigation-->
+            </div><!--end container-->
+        </header><!--end header-->
+    <!-- Navbar End -->
+</template>
+
+<script>
+	export default {
+		name: 'Nav',
+		data() {
+			return {
+				isOpen: false,
+	      	}
+		},
+		methods: {
+			//
+		}
+	}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <!-- Hero Start -->
+        <section class="main-slider">
+            <ul class="slides">
+                <li class="bg-slider bg-animation-left d-flex align-items-center" style="background-image:url('images/hotel/bg03.jpg')">
+                    <div class="container">
+                        <div class="row align-items-center">
+                            <div class="col-lg-7 col-md-7">
+                                <div class="title-heading position-relative mt-4" style="z-index: 1;">
+                                    <h1 class="heading mb-3">Welcome Landrick <br> A Stunning Hotel</h1>
+                                    <p class="para-desc">Launch your campaign and benefit from our expertise on designing and managing conversion centered bootstrap4 html page.</p>
+                                    <div class="mt-4 pt-2">
+                                        <a href="#bookroom" class="btn btn-primary mouse-down"><i class="mdi mdi-home-assistant"></i> Book now</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </section><!--end section-->
+        <!-- Hero End -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
